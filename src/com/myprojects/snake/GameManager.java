11a7ba6bbec7bc7ff7307d72b47989eb855398bc
nqriver <nqriver@gmail.com>
2021-09-15 14:57:34 +0200

    Game mechanics separated from game panel

---
+++ src/com/myprojects/snake/GameManager.java
@@ -0,0 +1,84 @@
+package com.myprojects.snake;
+
+import java.awt.*;
+import java.util.List;
+
+public class GameManager {
+
+    private Snake snake = new Snake();
+    private Food food = new Food(snake);
+    private boolean gameRunning;
+
+
+    public GameManager() {
+        gameRunning = true;
+    }
+
+    public boolean isGameRunning() {
+        return gameRunning;
+    }
+
+    private boolean checkCollisionWithBounds() {
+        var head = snake.getHead();
+        return  head.x < 0  ||
+                head.x == Board.X_FIELDS ||
+                head.y < 0 ||
+                head.y == Board.Y_FIELDS;
+    }
+
+    private boolean checkCollisionWithBody() {
+        var head = snake.getHead();
+        List<Point> tail = snake.getTail();
+        for (var part : tail) {
+            if (head.equals(part)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkCollisionWithFood() {
+        return snake.getHead().equals(food);
+    }
+
+    public void handleCollisions() {
+        if (checkCollisionWithBody() || checkCollisionWithBounds())
+            gameRunning = false;
+
+        if (checkCollisionWithFood()) {
+            food = new Food(snake);
+            snake.extend();
+        }
+    }
+
+    public void gameLoop() {
+        snake.move();
+        handleCollisions();
+    }
+
+    public String setScoreText() {
+        return "Score: " + snake.getSize();
+    }
+
+    public void renderGraphics(Graphics2D g) {
+        Board.draw(g);
+        food.draw(g);
+        snake.draw(g);
+    }
+
+    public void moveSnakeUp() {
+        snake.setDirection(Direction.UP);
+    }
+
+    public void moveSnakeDown() {
+        snake.setDirection(Direction.DOWN);
+    }
+
+    public void moveSnakeRight() {
+        snake.setDirection(Direction.RIGHT);
+    }
+
+    public void moveSnakeLeft() {
+        snake.setDirection(Direction.LEFT);
+    }
+}
